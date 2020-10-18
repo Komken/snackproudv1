3a19cc516030ff81810d6ef436d00e3416ccd6c3
nicholas@ingegneris.com <nicholas@ingegneris.com>
2020-10-18 21:17:55 +1100

    UI

--- lib/Home.dart
+++ lib/Home.dart
@@ -36,6 +36,8 @@ final List<Product> products = <Product>[
       'Chocolate Not Chocolate, Choc Truffles, 30g', 4, '3.png', 0),
   new Product('price_1Ha0N4GEyFEWKkD6ZThJu5td',
       'Botanika Bars, Lemon Cheese Cake, 40g', 4, '4.png', 0),
+  new Product('price_1Ha0N4GEyFEWKkD6ZThJu5td', 'Lemon Cheese Cake, 40g', 4,
+      '5.png', 0),
 ];
 
 MediaQueryData queryData;
@@ -57,119 +59,145 @@ class home extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           //Background
-
-          //Logo
-          Transform.translate(
-            offset: Offset(blockSizeWidth * 2, blockSizeHeight * 4),
-            child: Container(
-              width: blockSizeWidth * 17,
-              height: blockSizeHeight * 6,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/logo.png'),
-                  fit: BoxFit.fill,
+          Stack(
+            children: [
+              Transform.translate(
+                offset: Offset(blockSizeWidth * 0, blockSizeHeight * 0),
+                child: Container(
+                  decoration: new BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  width: blockSizeWidth * 99,
+                  height: blockSizeHeight * 12.5,
                 ),
               ),
-            ),
-          ),
-
-          //Top Container
-
-          Center(
-            child: Container(
-              color: Color(0xFFFFFF),
-              child: Text(
-                'Snacking at Westpac Level 24',
-                style: TextStyle(color: Colors.black, fontSize: 18),
+              Transform.translate(
+                offset: Offset(blockSizeWidth * 2, blockSizeHeight * 4),
+                //Logo
+                child: Container(
+                  width: blockSizeWidth * 17,
+                  height: blockSizeHeight * 6,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/logo.png'),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
               ),
-              alignment: Alignment(1.0, -0.9),
-            ),
+              //Top Container
+
+              Center(
+                child: Container(
+                  color: Color(0xFFFFFF),
+                  child: Text(
+                    'Snacking at Westpac Level 24',
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                  ),
+                  alignment: Alignment(1.0, -0.9),
+                ),
+              ),
+// yellow bar
+              Transform.translate(
+                offset: Offset(blockSizeWidth * 0.5, blockSizeHeight * 11),
+                child: Container(
+                  decoration: new BoxDecoration(
+                    color: Colors.yellow,
+                  ),
+                  width: blockSizeWidth * 99,
+                  height: blockSizeHeight * 2.5,
+                ),
+              ),
+            ],
           ),
+
+          Stack(children: [
+            ListView.separated(
+              padding: const EdgeInsets.fromLTRB(10, 100, 0, 100),
+              itemCount: products.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Row(children: [
+                  Container(
+                      width: blockSizeWidth * 95,
+                      decoration: new BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: new BorderRadius.circular(10),
+                      ),
+                      child: Row(children: [
+                        Container(
+                          height: 120.0,
+                          width: 120.0,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/${products[index].icon}'),
+                              fit: BoxFit.fill,
+                            ),
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Container(
+                              height: 50,
+                              width: 200,
+                              child: Text(' ${products[index].label}'),
+                            ),
+                            Container(
+                              height: 20,
+                              width: 200,
+                              child: Text('\$ ${products[index].price}'),
+                            )
+                          ],
+                        ),
+                        DropdownButton(
+                            value: products[index].quantity,
+                            items: [
+                              DropdownMenuItem(child: Text("0"), value: 0),
+                              DropdownMenuItem(child: Text("1"), value: 1),
+                              DropdownMenuItem(child: Text("2"), value: 2),
+                              DropdownMenuItem(child: Text("3"), value: 3),
+                              DropdownMenuItem(child: Text("4"), value: 4),
+                              DropdownMenuItem(child: Text("5"), value: 5),
+                              DropdownMenuItem(child: Text("6"), value: 6),
+                              DropdownMenuItem(child: Text("7"), value: 7),
+                              DropdownMenuItem(child: Text("8"), value: 8),
+                              DropdownMenuItem(child: Text("9"), value: 9),
+                              DropdownMenuItem(child: Text("10"), value: 10)
+                            ],
+                            onChanged: (value) {
+                              products[index].quantity = value;
+                              Navigator.of(context).push(
+                                MaterialPageRoute(builder: (context) => home()),
+                              );
+                              cart = 0;
+                              for (Product product in products) {
+                                cart =
+                                    (cart + (product.price * product.quantity));
+                                carts = ('\$' + cart.toString());
+                              }
+                            }),
+                      ]))
+                ]);
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(),
+            ),
+          ]),
 
           Transform.translate(
-            offset: Offset(blockSizeWidth * 0.5, blockSizeHeight * 11),
+            offset: Offset(blockSizeWidth * 0, blockSizeHeight * 93),
             child: Container(
               decoration: new BoxDecoration(
-                color: Colors.yellow,
+                color: Colors.grey[300],
               ),
               width: blockSizeWidth * 99,
-              height: blockSizeHeight * 2.5,
+              height: blockSizeHeight * 12.5,
             ),
           ),
 
-          ListView.separated(
-            padding: const EdgeInsets.fromLTRB(10, 100, 0, 0),
-            itemCount: products.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Row(children: [
-                Container(
-                    width: blockSizeWidth * 95,
-                    decoration: new BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: new BorderRadius.circular(10),
-                    ),
-                    child: Row(children: [
-                      Container(
-                        height: 120.0,
-                        width: 120.0,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage(
-                                'assets/images/${products[index].icon}'),
-                            fit: BoxFit.fill,
-                          ),
-                          shape: BoxShape.circle,
-                        ),
-                      ),
-                      Column(
-                        children: [
-                          Container(
-                            height: 50,
-                            width: 200,
-                            child: Text(' ${products[index].label}'),
-                          ),
-                          Container(
-                            height: 20,
-                            width: 200,
-                            child: Text('\$ ${products[index].price}'),
-                          )
-                        ],
-                      ),
-                      DropdownButton(
-                          value: products[index].quantity,
-                          items: [
-                            DropdownMenuItem(child: Text("0"), value: 0),
-                            DropdownMenuItem(child: Text("1"), value: 1),
-                            DropdownMenuItem(child: Text("2"), value: 2),
-                            DropdownMenuItem(child: Text("3"), value: 3),
-                            DropdownMenuItem(child: Text("4"), value: 4),
-                            DropdownMenuItem(child: Text("5"), value: 5),
-                            DropdownMenuItem(child: Text("6"), value: 6),
-                            DropdownMenuItem(child: Text("7"), value: 7),
-                            DropdownMenuItem(child: Text("8"), value: 8),
-                            DropdownMenuItem(child: Text("9"), value: 9),
-                            DropdownMenuItem(child: Text("10"), value: 10)
-                          ],
-                          onChanged: (value) {
-                            products[index].quantity = value;
-                            Navigator.of(context).push(
-                              MaterialPageRoute(builder: (context) => home()),
-                            );
-                            for (Product product in products) {
-                              cart =
-                                  (cart + (product.price * product.quantity));
-                              carts = ('\$' + cart.toString());
-                            }
-                          }),
-                    ]))
-              ]);
-            },
-            separatorBuilder: (BuildContext context, int index) =>
-                const Divider(),
-          ),
-
           Transform.translate(
-            offset: Offset(blockSizeWidth * 5, blockSizeHeight * 85),
+            offset: Offset(blockSizeWidth * 5, blockSizeHeight * 89),
             child: Container(
               decoration: new BoxDecoration(
                 color: Colors.blue,
@@ -180,7 +208,7 @@ class home extends StatelessWidget {
             ),
           ),
           Transform.translate(
-            offset: Offset(blockSizeWidth * 12.5, blockSizeHeight * 88.5),
+            offset: Offset(blockSizeWidth * 12.5, blockSizeHeight * 92.5),
             child: Container(
                 child: Text(
               '${carts}',
@@ -188,26 +216,29 @@ class home extends StatelessWidget {
             )),
           ),
           Transform.translate(
-            offset: Offset(blockSizeWidth * 67, blockSizeHeight * 86.75),
+            offset: Offset(blockSizeWidth * 67, blockSizeHeight * 90.75),
             child: Container(
               child: FlatButton(
                 onPressed: () async {
-                  // if (Item1Q > 0 || Item2Q > 0) {
-                  final sessionId = await Server().createCheckout(products);
+                  for (Product product in products) {
+                    if (product.quantity > 0) {
+                      final sessionId = await Server().createCheckout(products);
 
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (_) => CheckoutPage(
-                        sessionId: sessionId,
-                      ),
-                    ),
-                  );
-                  //    }
-                  /* Scaffold.of(context).showSnackBar(
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (_) => CheckoutPage(
+                            sessionId: sessionId,
+                          ),
+                        ),
+                      );
+                      //    }
+                      /* Scaffold.of(context).showSnackBar(
                     SnackBar(
                       content: Text('sessionID: $sessionId'),
                     ), 
                   ); */
+                    }
+                  }
                 },
                 child: Text(
                   'Pay Now',
